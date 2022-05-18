ee6a06fe4b9838c9d445b47c20f82c6f7a26a500
ishimwe-emma <ishimweemmanuel2005@gmail.com>
2022-05-18 05:52:53 +0200

    configured a react-app and tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+  colors: {},
+
+  fontFamily: {},
+
+  backgroundColors: {},
+  extend: {},
+  plugins: [],
+  } 
+}
