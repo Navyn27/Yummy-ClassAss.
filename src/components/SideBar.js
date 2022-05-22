b868a42c3356cf35eeb3057f605ff08b15503069
ishimwe-emma <ishimweemmanuel2005@gmail.com>
2022-05-22 14:19:40 +0200

    modeified the sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './../assets/logo.png'
 import { SiAirtable } from 'react-icons/si'
 import { FaFirstOrder } from 'react-icons/fa'
 import { MdOutlineViewInAr } from 'react-icons/md'
 import { HiViewGrid } from 'react-icons/hi'
-import { FiSettings } from 'react-icons/fi'
+import { MdDarkMode, MdLightMode } from 'react-icons/md'
 import { CgProfile } from 'react-icons/cg'
+import useDarkMode from '../Hooks/useDarkMode'
+import { Link } from 'react-router-dom'
 
 import Sidebarcomponent from './Sidebarcomponent'
 
 function Sidebar() {
+
+  const [theme, setTheme] = useDarkMode()
 
   const sidebarComponentsData1 = [
     {
@@ -30,29 +34,12 @@ function Sidebar() {
     },
   ]
 
-  const sidebarComponentsData2 = [
-    {
-      icon: <FiSettings />,
-      content: 'Settings'
-    },
-    {
-      icon: <CgProfile />,
-      content: 'Profile'
-    }
-  ]
-
-
   const sidebarComponents1 = sidebarComponentsData1.map((sidebarcomponentData, index)=>{
-    return <Sidebarcomponent key={index} icon={sidebarcomponentData.icon} name={sidebarcomponentData.content} link={sidebarcomponentData.content} />
+    return <Sidebarcomponent key={index} icon={sidebarcomponentData.icon} name={sidebarcomponentData.content} link={sidebarcomponentData.content}  />
   })
-
-  const sidebarComponents2 = sidebarComponentsData2.map((sidebarcomponentData, index)=>{
-    return <Sidebarcomponent key={index} icon={sidebarcomponentData.icon} name={sidebarcomponentData.content} />
-  })
-
 
   return (
-    <div className='w-[25rem] h-[100vh] bg-myred flex flex-col items-center justify-between fixed z-[1] '>
+    <div className='w-[25rem] h-[100vh] bg-myred dark:bg-[orange] flex flex-col items-center justify-between fixed z-[1] '>
       <div className='w-full h-[50%] flex flex-col items-center justify-around '>
         <div className='logo w-[15rem] h-fit flex items-center justify-start '>
             <img src={logo} alt="logo" />
@@ -62,7 +49,16 @@ function Sidebar() {
         </div>
       </div>
       <div className='w-full h-[20%]  flex flex-col items-center justify-center gap-[2rem]'>
-        {sidebarComponents2}
+        <div className='SidebarComponent w-[15rem] h-[3rem] flex items-center justify-start gap-[1rem] rounded-[.2rem]'>
+            <div className='text-[white] text-3xl font-bold'>{theme === 'dark' ? <MdDarkMode onClick={()=>setTheme(theme)}  /> : <MdLightMode onClick={()=>setTheme(theme)} />}</div>
+            <b className='text-[white]'>{theme === 'dark' ? 'Darkmode' : 'Lightmode'}</b>
+        </div>
+        <Link to='/profile'>
+          <div className='SidebarComponent w-[15rem] h-[3rem] flex items-center justify-start gap-[1rem] rounded-[.2rem]'>
+              <div className='text-[white] text-3xl font-bold'><CgProfile /></div>
+              <b className='text-[white]'>Profile</b>
+          </div>
+        </Link>
       </div>
     </div>
   )
